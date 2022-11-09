cf17789185d05ff8c9cd02df9c952bbf0374331c
bikashdahalgithub <bikashdahal991@gmail.com>
2022-11-09 06:56:25 +0545

    Array

---
+++ array/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int [,] vals = new int [4,2]{
+    {9,99},
+    {3,33},
+    {4,44},
+    {1,11},
+};
+  for (int i =0; i<4: i++){
+    for (int j = 0:j<2;j++)
+    {
+        Console.WriteLine.vals[i,j];
+    }
+  }
+  Console.ReadKey();
